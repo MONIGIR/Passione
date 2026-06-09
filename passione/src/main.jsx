980a0b3b80ab9308d102f01549217fa4f455ee2f
J4dys <jd.inzunza24@info.uas.edu.mx>
2026-06-09 15:50:21 -0700

    integracion de un dashboard admin(junto a su barra vertical)

--- passione/src/main.jsx
+++ passione/src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client'
 //import { LoginRegister } from "./modulos-jsx/login-register.jsx";
 //import './styles/login.css'
 import './modulos-jsx/barraLateral.jsx';
-import App from './modulos-jsx/userDashboard.jsx';
-
+//import App from './modulos-jsx/userDashboard.jsx';
 import './styles/user.css';
-
+import AdminDashboard from './modulos-jsx/AdminDashboard.jsx';
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <AdminDashboard />
     </StrictMode>
 )
