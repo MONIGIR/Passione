17e3eacc5a10b8d8612b2a6b3b40de1c6a2b2510
JACV04 <ja.calleros24@info.uas.edu.mx>
2026-06-09 19:42:24 -0700

    Seagrego AdminDasBoard

--- passione/src/main.jsx
+++ passione/src/main.jsx
@@ -4,11 +4,12 @@ import { createRoot } from 'react-dom/client'
 //import './styles/login.css'
 import './modulos-jsx/barraLateral.jsx';
 import App from './modulos-jsx/userDashboard.jsx';
+import DashboardInventario from './modulos-jsx/AdminDashBoard.jsx';
 
 import './styles/user.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <DashboardInventario />
     </StrictMode>
 )
